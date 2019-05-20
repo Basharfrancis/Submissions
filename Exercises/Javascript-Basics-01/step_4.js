138923572bc53ffab9a41af13b60fdf1719bd8f5
Bashar <basharfrancis16@gmail.com>
2019-05-20 11:24:18 +0300

    Strings concatenation 2

---
+++ Exercises/Javascript-Basics-01/step_4.js
@@ -0,0 +1,4 @@
+var yourName = prompt("Enter your Name");
+var surName = prompt("Enter your surName");
+var city = prompt ("Enter your City");
+alert ("Nom :" +yourName +  "\n" +"prenome :" +surName +"\n" +"ville :" +city);
