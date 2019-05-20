77fbd922a63babfd7144d0d79589e2ff6ddf23d1
Bashar <basharfrancis16@gmail.com>
2019-05-20 19:23:50 +0300

    Step 4

---
+++ Exercises/Javascript-Basics-02/step 4/step_4.js
@@ -0,0 +1,18 @@
+var x = document.querySelector("input");
+
+var y = document.querySelector("button");
+y.addEventListener("click",hsadaas);
+function hsadaas(){
+    var yes;
+    if(confirm("yes or no")){
+        yes = document.getElementById("name").value = ""; 
+        document.getElementById("surname").value = ""; 
+        document.getElementById("city").value = ""; 
+       
+
+    }
+    
+
+
+}
+
