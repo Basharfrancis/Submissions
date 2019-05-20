94d19cce4527c7c036731bcd9a7a8d2cf7e856cd
Bashar <basharfrancis16@gmail.com>
2019-05-20 13:42:27 +0300

    Multiple operations

---
+++ Exercises/Javascript-Basics-01/step_7.js
@@ -0,0 +1,15 @@
+var a= parseInt(prompt("enter your shoe size"));
+var b = parseInt(prompt("enter your birth"));
+var z;
+function myfunction(a,b){
+  z=a*2
+  z=z+5
+  z=z*50
+  z=z-b
+  z=z+1766
+
+    return z;
+    
+}
+myfunction();
+alert(myfunction(a,b));
