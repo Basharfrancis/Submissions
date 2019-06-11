e13e9a4729d0cc155b4a99ca88fd8d0b3e7eedae
Bashar <basharfrancis16@gmail.com>
2019-06-11 17:40:31 +0300

    Step3

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -5,6 +5,17 @@ const app = express() ;
 app.get('/', (req,res) => {
 res.send('ok');
 });
+app.get('/test',(req,res) =>{
+res.send({status:200, message:"ok"})
+})
+var today = new Date()
+var TIME = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+app.get('/time',(req,res)=>{
+    res.send({
+        status:200,
+         message:TIME})
+})
+
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`)
